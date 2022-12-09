e8fbb50cae7f59633b06649aeb5076507ad4da52
unknown <elenelelashvili6@gmail.com>
2022-12-09 11:27:37 +0400

    StartProject

---
+++ src/main/java/ge/ibsu/demo/controllers/TestController.java
@@ -0,0 +1,24 @@
+package ge.ibsu.demo.controllers;
+
+
+import ge.ibsu.demo.dto.Person;
+import ge.ibsu.demo.dto.Test;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value="api/test")
+public class TestController {
+    @ResponseBody
+    @RequestMapping(value="/call",method= RequestMethod.GET, produces={"application/json"})
+    public String test(){
+        return "Hello";
+    }
+
+    @ResponseBody
+    @RequestMapping(value="/person",method=RequestMethod.POST,produces = {"application/json"})
+    public Test testPost(@RequestBody Person rd){
+        Test t= new Test();
+        t.setFullName(rd.getFirstName()+" "+rd.getLastName());
+        return t;
+    }
+}
---
+++ src/main/java/ge/ibsu/demo/dto/Test.java
@@ -0,0 +1,12 @@
+package ge.ibsu.demo.dto;
+
+public class Test {
+    private String fullName;
+    public String getFullName(){
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
