735b131d76a63aed49c17e2e5615f81ce16bb42b
unknown <elenelelashvili6@gmail.com>
2022-12-09 10:02:23 +0400

    StartProject

---
+++ src/main/java/ge/ibsu/demo/DembiApplication.java
@@ -0,0 +1,13 @@
+package ge.ibsu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DembiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DembiApplication.class, args);
+	}
+
+}
